a859fe88ef5263823f654796e4a2dadb1925db78
krspra <pradeepyd708@gmail.com>
2024-10-01 17:45:17 +0530

    undo render.com deployment process.

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,9 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.route');
-const path=require('path');
 
 dotenv.config({});
 const server = express();
-const _dirname=path.resolve()
-
 
 // Importing from files
 const connectDB = require('./utils/db');
@@ -26,10 +23,6 @@ server.use(cors(corsOptions));
 
 // API handling
 server.use('/api/user', userRouter);
-server.use(express.static(path.join(_dirname,"/frontend/dist")))
-server.get('*',(req,res)=>{
-    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
-})
 
 //-------------------------------
 const PORT = process.env.PORT || 3000;
